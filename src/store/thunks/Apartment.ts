bf1ceb5b9915c893b311dd0f0fee9a6537387724
6-ON <aminhatim6@gmail.com>
2023-12-14 11:41:03 +0100

    Refactor thunks to handle errors and display toast messages

--- src/store/thunks/Apartment.ts
+++ src/store/thunks/Apartment.ts
@@ -2,40 +2,61 @@ import { batch } from 'react-redux'
 import { ApartmentsService } from '../../api'
 import { addApartment, changeMonth, changeStatus, deleteApartment, getApartments, updateApartment } from '../actions'
 import { AppDispatch } from '../store'
+import { toast } from '../../utils/toast'
 
 export const getApartmentsThunk = () => async (dispatch: AppDispatch) => {
-	dispatch(changeStatus('loading'))
-	const result = await ApartmentsService.getAll()
-	batch(() => {		
-		dispatch(getApartments(result))
-		dispatch(changeStatus('idle'))
-	})
+	try {
+		dispatch(changeStatus('loading'))
+		const result = await ApartmentsService.getAll()
+		batch(() => {
+			dispatch(getApartments(result))
+			dispatch(changeStatus('idle'))
+		})
+	} catch (err) {
+		toast({ title: err.message, status: 'error' })
+	}
 }
 export const apartmentsByMonthThunk = (m: Month) => async (dispatch: AppDispatch) => {
-	batch(() => {
-		console.log(m);
-	
-		dispatch(changeMonth(m)) // change month in store to use it for payments
-		dispatch(changeStatus('loading'))
-	})
-	const result = await ApartmentsService.getAll(m)
-	batch(() => {		
-		dispatch(getApartments(result))
-		dispatch(changeStatus('idle'))
-	})
+	try {
+		batch(() => {
+			console.log(m)
+
+			dispatch(changeMonth(m)) // change month in store to use it for payments
+			dispatch(changeStatus('loading'))
+		})
+		const result = await ApartmentsService.getAll(m)
+		batch(() => {
+			dispatch(getApartments(result))
+			dispatch(changeStatus('idle'))
+		})
+	} catch (err) {
+		toast({ title: err.message, status: 'error' })
+	}
 }
 
 export const deleteApartmentThunk = (_id: string) => async (dispatch: AppDispatch) => {
-	await ApartmentsService.delete(_id)
-	dispatch(deleteApartment(_id))
+	try {
+		await ApartmentsService.delete(_id)
+		dispatch(deleteApartment(_id))
+	} catch (err) {
+		toast({ title: err.message, status: 'error' })
+	}
 }
 
 export const addApartmentThunk = (apartment: TApartmentForm) => async (dispatch: AppDispatch) => {
-	const result = await ApartmentsService.create(apartment)
-	dispatch(addApartment(result))
+	try {
+		const result = await ApartmentsService.create(apartment)
+		dispatch(addApartment(result))
+	} catch (err) {
+		toast({ title: err.message, status: 'error' })
+	}
 }
 export const updateApartmentThunk =
 	(_id: string, updates: Partial<TApartmentForm>) => async (dispatch: AppDispatch) => {
-		const result = await ApartmentsService.update(_id, updates)
-		dispatch(updateApartment(_id, updates))
+		try {
+			const result = await ApartmentsService.update(_id, updates)
+			dispatch(updateApartment(_id, updates))
+		} catch (err) {
+			toast({ title: err.message, status: 'error' })
+		}
 	}
--- src/store/thunks/Facture.ts
+++ src/store/thunks/Facture.ts
@@ -5,15 +5,25 @@ import { FactureAction } from '../types'
 import { FactureService } from '../../api'
 import { createFacture, getFactures } from '../actions'
 import { GetRootState } from '../store'
+import { toast } from '../../utils/toast'
 
 export const getFacturesThunk = () => async (dispatch: Dispatch<FactureAction>) => {
-	const result = await FactureService.getRecent()
-	dispatch(getFactures(result))
+	try {
+		const result = await FactureService.getRecent()
+		dispatch(getFactures(result))
+	} catch (err) {
+		toast({ title: err.message, status: 'error' })
+	}
 }
 
-export const createFactureThunk = (apartmentId:string) => async (dispatch: Dispatch<FactureAction>,getState:GetRootState) => {
-	const month = getState().apartment.selectedMonth
-	console.log(month,apartmentId);
-	// const result = await FactureService.create()
-	// dispatch(createFacture(result))
-}
+export const createFactureThunk =
+	(apartmentId: string) => async (dispatch: Dispatch<FactureAction>, getState: GetRootState) => {
+		try {
+			const month = getState().apartment.selectedMonth
+			console.log(month, apartmentId)
+			// const result = await FactureService.create()
+			// dispatch(createFacture(result))
+		} catch (err) {
+			toast({ title: err.message, status: 'error' })
+		}
+	}
