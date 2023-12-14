a586ee0ec4dc480e31c157dd42fae245623402b1
6-ON <aminhatim6@gmail.com>
2023-12-14 10:52:58 +0100

    Update axios configuration in main.ts add response interceptor

--- src/api/config/main.ts
+++ src/api/config/main.ts
@@ -1,6 +1,22 @@
-import axios from 'axios'
+import axios, { AxiosError, AxiosResponse } from 'axios'
 
 export const mainInstance = axios.create({
 	baseURL: process.env.API_URL,
-	headers: {"Content-Type":"application/json","Accept":"application/json"}
+	headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
 })
+
+mainInstance.interceptors.response.use(
+	(res: AxiosResponse) => res,
+	(error: AxiosError) => {
+		console.error('interceptor error', error)
+
+		if (error.response) {
+			return Promise.reject(error.response.data)
+		} else {
+			return Promise.reject({
+				message: 'Something went wrong',
+				error,
+			})
+		}
+	}
+)
