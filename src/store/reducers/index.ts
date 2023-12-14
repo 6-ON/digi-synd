a0a8a997bbf1b8129eae0148881462754a849af7
6-ON <aminhatim6@gmail.com>
2023-12-14 11:19:15 +0100

    Add authReducer to rootReducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,10 +1,12 @@
 import apartmentReducer from './ApartmentReducer'
+import authReducer from './AuthReducer'
 import facturesReducer from './FacturesReducer'
 import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
 	apartment: apartmentReducer,
-	factures: facturesReducer
+	factures: facturesReducer,
+	auth: authReducer,
 })
 
 export default rootReducer
