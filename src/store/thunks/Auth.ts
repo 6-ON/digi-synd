8414df029040cb6f44329249c6b7661b65c71166
6-ON <aminhatim6@gmail.com>
2023-12-17 14:04:42 +0100

    Add login and logout functionality with navigation

--- src/store/thunks/Auth.ts
+++ src/store/thunks/Auth.ts
@@ -1,16 +1,35 @@
+import { NavigateFunction, useNavigate } from 'react-router-dom'
 import { AuthService } from '../../api/auth'
 import { toast } from '../../utils/toast'
-import { setUser } from '../actions'
+import { setUser, unsetUser } from '../actions'
 import { AppDispatch } from '../store'
 
 export const login =
-	({ email, password }: TAuthForm) =>
+	({ email, password }: TAuthForm, navigate: NavigateFunction) =>
 	async (dispatch: AppDispatch) => {
 		try {
 			const result = await AuthService.login(email, password)
 			dispatch(setUser(result))
+			navigate('/')
 			toast({ title: 'Logged in', status: 'success' })
 		} catch (error) {
 			toast({ title: error.message, status: 'error' })
 		}
 	}
+export const getUser = () => async (dispatch: AppDispatch) => {
+	try {
+		const result = await AuthService.me()
+		dispatch(setUser(result))
+	} catch (error) {
+		console.log(error);
+		
+		toast({ title: error.message, status: 'error' })
+	}
+}
+export const logout = (navigate: NavigateFunction) => (dispatch: AppDispatch) => {
+	dispatch(unsetUser())
+	localStorage.removeItem('access_token')
+	localStorage.removeItem('refresh_token')
+	localStorage.removeItem('user')
+	navigate('/')
+}
