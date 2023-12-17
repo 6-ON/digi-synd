2497d6c34111e5ec6b46669c40512d27b1a7ecee
6-ON <aminhatim6@gmail.com>
2023-12-17 14:10:40 +0100

    Refactor App component and add getUser thunk
    This commit refactors the App component by adding a useEffect hook to dispatch the getUser thunk when the component mounts. It also renames the App component to MainApp.

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,26 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
 import theme from './theme'
-import { Provider } from 'react-redux'
+import { Provider, useDispatch } from 'react-redux'
 import { store } from './store'
+import { useEffect } from 'react'
+import { getUser } from './store/thunks'
 
 const App = () => {
+	const dispatch = useDispatch()
+	useEffect(() => {
+		if (localStorage.getItem('user')) dispatch<any>(getUser())
+	}, [dispatch])
+	return <RouterProvider router={router} />
+}
+const MainApp = () => {
 	return (
 		<Provider store={store}>
 			<ChakraProvider theme={theme}>
-				<RouterProvider router={router} />
+				<App />
 			</ChakraProvider>
 		</Provider>
 	)
 }
 
-export default App
+export default MainApp
