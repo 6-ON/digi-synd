8cdc11c4416b769c99fd7b1871811c31e494de37
6-ON <aminhatim6@gmail.com>
2023-12-17 13:36:56 +0100

    Update routes in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,26 @@
-import { RouteObject, createBrowserRouter } from 'react-router-dom'
+import { Navigate, RouteObject, createBrowserRouter } from 'react-router-dom'
 import { Syndicate, Auth, Dashboard } from '../pages'
 import { EmptyLayout } from '../layouts'
 import SidebarLayout from '../layouts/SidebarLayout'
+import RequireRoles from './RequireRole'
+import { Entry } from './Entry'
+import PreventAuth from './PreventAuth'
 
 const routes: RouteObject[] = [
 	{
+		path: '/',
+		Component: Entry,
+	},
+	{
 		Component: EmptyLayout,
 		children: [
-			{ path: '/panel', Component: Syndicate },
-			{ path: '/auth', Component: Auth },
+			{ path: '/panel', Component: () => RequireRoles(Syndicate, ['admin']) },
+			{ path: '/auth', Component: () => PreventAuth(Auth) },
 		],
 	},
 	{
 		Component: SidebarLayout,
-		children: [{ path: '/dashboard', Component: Dashboard }],
+		children: [{ path: '/dashboard', Component: () => RequireRoles(Dashboard, ['syndicate']) }],
 	},
 ]
 
