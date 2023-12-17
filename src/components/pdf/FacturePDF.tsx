7eb8e98cee0a8d3dbf218bf6bd8edda2c6f71edb
6-ON <aminhatim6@gmail.com>
2023-12-17 13:24:56 +0100

    Add FacturePDF component to generate PDF invoices

---
+++ src/components/pdf/FacturePDF.tsx
@@ -0,0 +1,33 @@
+import { Document, Page, StyleSheet, Text } from '@react-pdf/renderer'
+import React, { FC } from 'react'
+type FacturePDFProps = {
+	facture: TFactureRecord
+}
+const styles = StyleSheet.create({
+	page: {
+		backgroundColor: '#E4E4E4',
+	},
+	section: {
+		margin: 10,
+		padding: 10,
+	},
+	heading: {
+		fontSize: 24,
+		textAlign: 'center',
+		marginBottom: 20,
+	},
+})
+export const FacturePDF: FC<FacturePDFProps> = ({ facture }) => {
+	return (
+		<Document>
+			<Page size={'A4'} style={styles.page}>
+				{/* design the page */}
+				<Text style={styles.heading}>Facture</Text>
+				<Text>Owner name :{facture.apartment.owner.name}</Text>
+				<Text>Facture ID : {facture._id}</Text>
+				<Text>Mois : {new Date(facture.month).toLocaleString().slice(3, 10)}</Text>
+				<Text>Creé en : {new Date(facture.createdAt).toLocaleDateString()}</Text>
+			</Page>
+		</Document>
+	)
+}
