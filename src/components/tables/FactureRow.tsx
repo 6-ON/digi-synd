6cad0990b4f53b36b26923ffbcf6d0afee46e640
6-ON <aminhatim6@gmail.com>
2023-12-17 13:26:52 +0100

    Add PDF download functionality to FactureRow component

--- src/components/tables/FactureRow.tsx
+++ src/components/tables/FactureRow.tsx
@@ -1,21 +1,30 @@
 import { CopyIcon } from '@chakra-ui/icons'
 import { Box, Button, Flex, Text, VStack } from '@chakra-ui/react'
+import ReactPDF, { PDFDownloadLink } from '@react-pdf/renderer'
 import { FC } from 'react'
+import { FacturePDF } from '../pdf'
 type FactureRowProps = {
 	facture: TFactureRecord
 }
 const FactureRow: FC<FactureRowProps> = ({ facture }) => {
+	console.log(facture)
+
 	return (
 		<Box bg="gray.50" rounded={10} px="6" py="8" mb={10}>
 			<Flex justify="space-between">
-				<VStack color="gray.500" align="start">
-					<Text fontSize={"xl"} fontWeight="bold">{facture.apartment.owner.name}</Text>
+				<VStack fontSize="sm" color="gray.500" align="start">
+					<Text fontSize="lg" fontWeight="bold">
+						{facture.apartment.owner.name}
+					</Text>
 					<Text>Facture ID : {facture._id}</Text>
-					<Text>Creé en : {facture.createdAt}</Text>
+					<Text>Mois : {new Date(facture.month).toLocaleString().slice(3, 10)}</Text>
+					<Text>Creé en : {new Date(facture.createdAt).toLocaleDateString()}</Text>
 				</VStack>
-				<Button variant={'ghost'} leftIcon={<CopyIcon />}>
-					PDF
-				</Button>
+				<PDFDownloadLink document={<FacturePDF facture={facture} />} fileName={`${facture._id}.pdf`}>
+					<Button variant={'ghost'} leftIcon={<CopyIcon />}>
+						PDF
+					</Button>
+				</PDFDownloadLink>
 			</Flex>
 		</Box>
 	)
